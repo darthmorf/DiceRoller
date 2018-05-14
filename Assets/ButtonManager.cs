c4cc32e4ceac05e795d79671769e9e9fe7239466
darthmorf <sampoirier@email.com>
2018-05-14 11:03:48 +0100

    Implemented D8 and spawn UI

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+	public GameObject d6;
+	public GameObject d8;
+	public Canvas SpawnButtons;
+
+	private List<GameObject> dice = new List<GameObject>();
+	private float xPos = 0f;
+	private float zPos = 0f;
+
+	private bool rollButtonPressed = false;
+
+	public void ToggleSpawnUI()
+	{
+		SpawnButtons.enabled = !SpawnButtons.enabled;
+	}
+
+	public void SpawnD6()
+	{
+		SpawnDie(d6);
+	}
+
+	public void SpawnD8()
+	{
+		SpawnDie(d8);
+	}
+
+	private void SpawnDie(GameObject diceType)
+	{
+		GameObject go = Instantiate(diceType, new Vector3(xPos, 5, zPos), Quaternion.identity);
+		dice.Add(go);
+
+		if (xPos == 0) xPos += 3f;
+
+		else if (xPos > 0) xPos = (xPos * -1);
+
+		else if (xPos < 0) xPos = (xPos * -1) + 3f;
+
+		if (dice.Count % 18 == 0)
+		{
+			xPos = 0;
+			if (zPos == 0) zPos += 3f;
+
+			else if (zPos > 0) zPos = (zPos * -1);
+
+			else if (zPos < 0) zPos = (zPos * -1) + 3f;
+		}
+	}
+
+	public void RollPress()
+	{
+		rollButtonPressed = true;
+	}
+
+	public void RollRelease()
+	{
+		rollButtonPressed = false;
+	}
+
+	private void Update()
+	{
+		if(rollButtonPressed)
+		{
+			foreach (GameObject go in dice)
+			{
+				go.GetComponent<Roll>().RollDie();
+			}
+		}
+	}
+}
--- Assets/Roll.cs
+++ Assets/Roll.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Roll : MonoBehaviour {
+public class Roll : MonoBehaviour
+{
 
 	public GameObject die;
 	public float forceMin;
 	public float forceMax;
+	public Button rollButton;
 
 	Vector3 startPos;
 
@@ -16,18 +19,21 @@ public class Roll : MonoBehaviour {
 		startPos = die.transform.position;
 	}
 
-	private void Update()
+	public void RollDie()
 	{
-		if(Input.GetKey("space") || Input.touchCount > 0)
-		{
-			// Reset
-			die.GetComponent<Rigidbody>().velocity = Vector3.zero;
-			die.transform.position = startPos;
+		Debug.Log("Rolling");
+		// Reset
+		die.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		die.transform.position = startPos;
 
-			// Random orientation and Force
-			die.transform.rotation = Random.rotation;
-			die.GetComponent<Rigidbody>().useGravity = true;
-			die.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(forceMin, forceMax), Random.Range(forceMin, forceMax), Random.Range(forceMin, forceMax)));
-		}
+		// Random orientation and Force
+		die.transform.rotation = Random.rotation;
+		die.GetComponent<Rigidbody>().useGravity = true;
+		die.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(forceMin, forceMax) * RandomSign(), 0, Random.Range(forceMin, forceMax) * RandomSign()));
+	}
+
+	private int RandomSign()
+	{
+		return Random.value< .5? 1 : -1;
 	}
 }
