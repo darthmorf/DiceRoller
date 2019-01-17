916352ca90ea02cda382b1ac22cab3cb5de95084
darthmorf <sampoirier@email.com>
2019-01-17 21:20:18 +0000

    Determining rotation of a D4

--- DiceRoller/Assets/Scripts/RotationManager.cs
+++ DiceRoller/Assets/Scripts/RotationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RotationManager : MonoBehaviour
@@ -13,6 +14,10 @@ public class RotationManager : MonoBehaviour
 
         switch (diceType)
         {
+            case Assets.Scripts.Utilities.Dice.D4:
+                print(GetD4Value(rotation));
+                return;
+
             case Assets.Scripts.Utilities.Dice.D6:
                 print(GetD6Value(rotation));
                 return;
@@ -22,12 +27,41 @@ public class RotationManager : MonoBehaviour
         }
 	}
 
+    int GetD4Value (Vector3 rawRotation)
+    {
+        Vector3 roundedRotation = new Vector3(
+           RoundD4x(rawRotation.x),
+           rawRotation.y,
+           RoundD4z(rawRotation.z));
+
+        if (roundedRotation.x == 19.5f && roundedRotation.z == 30)
+        {
+            return 4;
+        }
+        else if (roundedRotation.x == 19.5f && roundedRotation.z == 150)
+        {
+            return 2;
+        }
+        else if (roundedRotation.x == 19.5f && roundedRotation.z == 270)
+        {
+            return 3;
+        }
+        else if (roundedRotation.x == 270)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
     int GetD6Value (Vector3 rawRotation)
     {
         Vector3 roundedRotation = new Vector3(
-            Assets.Scripts.Utilities.ToNearest90(rawRotation.x),
-            Assets.Scripts.Utilities.ToNearest90(rawRotation.y),
-            Assets.Scripts.Utilities.ToNearest90(rawRotation.z));
+            RoundD6(rawRotation.x),
+            rawRotation.y,
+            RoundD6(rawRotation.z));
 
         if (roundedRotation.x == 0 && roundedRotation.z == 0)
         {
@@ -56,6 +90,80 @@ public class RotationManager : MonoBehaviour
         else if (roundedRotation.x == 0 && roundedRotation.z == 270)
         {
             return 5;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+
+
+   float RoundD4x (float rawValue)
+    {
+        if (rawValue <= 144.75)
+        {
+            return 19.5f;
+        }
+        else if (rawValue <= 324.75)
+        {
+            return 270f;
+        }
+        else if (rawValue <= 360)
+        {
+            return 19.5f;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    int RoundD4z (float rawValue)
+    {
+        if (rawValue <= 90)
+        {
+            return 30;
+        }
+        else if (rawValue <= 210)
+        {
+            return 150;
+        }
+        else if (rawValue <= 330)
+        {
+            return 270;
+        }
+        else if (rawValue <= 360)
+        {
+            return 30;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    int RoundD6(float rawValue)
+    {
+        if (rawValue <= 45)
+        {
+            return 0;
+        }
+        else if (rawValue <= 135)
+        {
+            return 90;
+        }
+        else if (rawValue <= 225)
+        {
+            return 180;
+        }
+        else if (rawValue <= 315)
+        {
+            return 270;
+        }
+        else if (rawValue <= 360)
+        {
+            return 0;
         }
         else
         {
--- DiceRoller/Assets/Scripts/Utilities.cs
+++ DiceRoller/Assets/Scripts/Utilities.cs
@@ -8,33 +8,5 @@ namespace Assets.Scripts
     public class Utilities
     {
         public enum Dice { D4, D6, D8, D10, D12, D20, D100 };
-
-        public static int ToNearest90 (float rawValue)
-        {
-            if (rawValue < 0 + 45)
-            {
-                return 0;
-            }
-            else if (rawValue < 90 + 45)
-            {
-                return 90;
-            }
-            else if (rawValue < 180 + 45)
-            {
-                return 180;
-            }
-            else if (rawValue < 270 + 45)
-            {
-                return 270;
-            }
-            else if (rawValue < 360)
-            {
-                return 0;
-            }
-            else
-            {
-                return -1;
-            }
-        }
     }
 }
